144396ada2392e4cad8524e0d45e25ecdc9a0cf6
n-ono <sample@example.com>
2019-12-02 21:01:14 +0900

    初回のコミット

---
+++ src/main/kotlin/com/example/jrpricing/JrPricingApplication.kt
@@ -0,0 +1,11 @@
+package com.example.jrpricing
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JrPricingApplication
+
+fun main(args: Array<String>) {
+	runApplication<JrPricingApplication>(*args)
+}
